515ce3a270f15ba9fd6079eb9ee29051b7e4839e
AXAz0r <thealeksaradovic@gmail.com>
2018-01-16 10:38:16 +0100

    Remove Constant Glitches

--- src/pages/Sigma.vue
+++ src/pages/Sigma.vue
@@ -3,7 +3,7 @@
     <section class="hero fadeIn" >
       <div id="snow">
         <div class="hero-body has-text-centered animated fadeIn">
-          <img id="sigma-logo" height="390px" class="logo animated fadeIn" src="./../assets/logo_sigma_light.png">
+          <img id="sigma-logo" height="390px" class="logo animated fadeIn" src="https://i.imgur.com/ih3P3Wa.png">
           <h1 class="title animated fadeIn">This is <strong id="sigma-name">Sigma</strong>, the Database Giant.</h1>
           <h2 class="subtitle animated fadeIn">A bot made to bring knowledge to your Discord server.</h2>
           <a class="button animated fadeIn" :href=links.invite target="_blank">Add to Discord</a>
@@ -63,74 +63,7 @@ export default {
       this.stats.push({label: 'Messages processed', value: data.events.message, icon: 'message-square'})
       this.stats.push({label: 'Songs Played', value: data.special.songs_played, icon: 'play'})
     })
-    setInterval(glitcher, 200)
-    setInterval(imageGlitcher, 200)
   }
-}
-function glitchify (txt) {
-  var glitchText = ''
-  var unicodes = [
-    '\u0315', '\u031b', '\u0340', '\u0341',
-    '\u0358', '\u0321', '\u0322', '\u0327',
-    '\u0328', '\u0334', '\u0335', '\u0336',
-    '\u034f', '\u035c', '\u035d', '\u035e',
-    '\u035f', '\u0360', '\u0362', '\u0338',
-    '\u0337', '\u0361', '\u0489'
-  ]
-  function isGlitch (c) {
-    var i
-    for (i = 0; i < unicodes.length; i++) {
-      if (c === unicodes[i]) {
-        return true
-      }
-      return false
-    }
-  }
-  for (var i = 0; i < txt.length; i++) {
-    if (isGlitch(txt.substr(i, 1))) {
-      continue
-    }
-    glitchText += txt.substr(i, 1)
-    var numMid = Math.floor(Math.random() * 4)
-    for (var j = 0; j < numMid; j++) {
-      glitchText += unicodes[Math.floor(Math.random() * unicodes.length)]
-    }
-  }
-  return glitchText
-}
-function glitcher () {
-  var txtRoll = Math.floor(Math.random() * 10)
-  var txt = 'Sigma'
-  if (txtRoll === 0) {
-    txt = 'Monika'
-  }
-  var glitchText = glitchify(txt)
-  var glitchCodeName = glitchify('Monika')
-  $('#sigma-name').text(glitchText)
-  $('#sigma-codename').text('Codename ' + glitchCodeName)
-}
-function imageGlitcher () {
-  var normalImg = ['https://i.imgur.com/B3FHWir.png']
-  var glitchImages = [
-    'https://i.imgur.com/62GdgTa.png',
-    'https://i.imgur.com/SEsGUSj.png',
-    'https://i.imgur.com/AP3ZAtW.png',
-    'https://i.imgur.com/ih3P3Wa.png'
-  ]
-  var monikaImages = [
-    'https://i.imgur.com/wJazxfj.png',
-    'https://i.imgur.com/6qqfqC7.png',
-    'https://i.imgur.com/eKYJ6QO.png'
-  ]
-  var imgArrayRoll = Math.floor(Math.random() * 10)
-  var imgArray = normalImg
-  if (imgArrayRoll === 0) {
-    imgArray = monikaImages
-  } else if (imgArrayRoll <= 5) {
-    imgArray = glitchImages
-  }
-  var imgChoice = imgArray[Math.floor(Math.random() * imgArray.length)]
-  $('#sigma-logo').attr('src', imgChoice)
 }
 </script>
 
